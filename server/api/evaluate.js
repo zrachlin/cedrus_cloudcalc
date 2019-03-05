61db18f32efbeecc24851e39386c016192670dcd
Zach Rachlin <zachrach@gmail.com>
2019-03-04 22:33:24 -0500

    comment for securitizing eval expression in /api/evaluate route

--- server/api/evaluate.js
+++ server/api/evaluate.js
@@ -4,7 +4,9 @@ module.exports = router;
 router.get('/', async (req, res, next) => {
   try {
     const expressionStr = req.query.expression;
+    //do more robust checks to determine if expression is valid/safe
     const result = eval(expressionStr);
+
     res.send({ result });
   } catch (error) {
     res.send('invalid expression');
